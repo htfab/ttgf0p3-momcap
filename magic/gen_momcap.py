3d15bf6e10746c07e031954cc2723c99db58ff2a
htfab <mpw@htamas.net>
2026-06-19 18:34:27 +0200

    try fixing Vn.2b violation

--- magic/gen_momcap.py
+++ magic/gen_momcap.py
@@ -20,17 +20,19 @@ for layer in ("metal1", "via1", "metal2", "via2", "metal3", "via3", "metal4"):
             else:
                 print(f"rect 56 {28*(2*i)} {28*(2*WIDTH)} {28*(2*i+1)}")
 
-    elif layer == "via1":
-        print(f"rect 1 6 27 {28*(2*HEIGHT+1)-6}")
-        print(f"rect {28*(2*WIDTH)+1} 6 {28*(2*WIDTH+1)-1} {28*(2*HEIGHT+1)-6}")
-        for i in range(HEIGHT+1):
-            print(f"rect 62 {28*(2*i)+1} {28*(2*WIDTH-1)-6} {28*(2*i+1)-1}")
-
     elif layer.startswith("via"):
-        print(f"rect 0 5 28 {28*(2*HEIGHT+1)-5}")
-        print(f"rect {28*(2*WIDTH)} 5 {28*(2*WIDTH+1)} {28*(2*HEIGHT+1)-5}")
-        for i in range(HEIGHT+1):
-            print(f"rect 61 {28*(2*i)} {28*(2*WIDTH-1)-5} {28*(2*i+1)}")
+        for j in range(HEIGHT+1):
+            for i in range(WIDTH+1):
+                if i == 1 or i == WIDTH-1:
+                    continue
+                if (i == 0 or i == WIDTH) and (j == 0 or j == HEIGHT):
+                    continue
+                if (i+j) % 2 == 0:
+                    continue
+                if layer == "via1":
+                    print(f"rect {28*(2*i)+1} {28*(2*j)+1} {28*(2*i+1)-1} {28*(2*j+1)-1}")
+                else:
+                    print(f"rect {28*(2*i)} {28*(2*j)} {28*(2*i+1)} {28*(2*j+1)}")
 
     else:
         raise NotImplementedError
